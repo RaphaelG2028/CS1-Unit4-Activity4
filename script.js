76e046d43c60c329b4bfac6c0a6d59dfc15648af
RaphaelG2028 <Raphael.goldstein2028@gbwl.org>
2026-05-06 18:59:04 +0000

    did button function

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
-console.log("hello world");
+// Boolean variable to track mode
+let DarkMode = false;
+
+/* NEW FUNCTION: document.querySelector
+   Lets you select any html element using its tag, class, or id.
+   Use const (instead of let) because were storing a REFERENCE to part of the HTML page. 
+*/
+
+const body = document.querySelector('body'); 
+const toggleBtn = document.querySelector('#toggle');
+const heading = document.querySelector('#main-heading');
+const description = document.querySelector('#description');
+
+//now we can change styles & attributes for these elements 
+heading.style.color = "darkblue"; //JS wrote CSS code...
+heading.textContent = "Light MOde & Dark Mode Website"
+
+// Define what happens when the button is clicked
+function toggleMode() {
+   console.log("function triggered");
+   // 1. flip the boolean value
+   DarkMode = !DarkMode; //if it was false, it becomes true. If it was true, it becomes false.
+   console.log(DarkMode);
+
+      // 2. conditionally apply stules based on mode
+      if (DarkMode == true) {
+         console.log("applu dark mode styles");
+         body.style.backgroundColor = "black";
+         heading.style.color = "lightgrey";
+         description.style.color = "white";
+         description.textContent = "Dark mode is on ";
+         toggleBtn.textContent = "Switch to Light Mode";
+         toggleBtn.style.background = "purple";
+
+      }
+      else {
+         console.log("apply light mode styles");
+         body.style.backgroundColor = "white";
+         heading.style.color = "darkblue";
+         description.style.color = "black";
+         description.textContent = "Light mode is on";
+         toggleBtn.textContent = "Switch to Dark Mode";
+         toggleBtn.style.backgroundColor = "lightblue";
+      }
+}
+// Attach the function to the button's click event 
+toggleBtn.addEventListener('click', toggleMode);
